8492f1950c097dfb7da868128e2a8f04454d522a
Mlie <emipa606@users.noreply.github.com>
2020-04-21 21:41:30 +0200

    Added two more soundpacks and setting to select

--- Source/RealisticHumanSounds/HarmonyPatches.cs
+++ Source/RealisticHumanSounds/HarmonyPatches.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 using Verse;
 using Verse.Sound;
 
-namespace RealisticHumanSoundsPatch
+namespace RealisticHumanSounds
 {
 
     [StaticConstructorOnStartup]
@@ -25,14 +25,29 @@ namespace RealisticHumanSoundsPatch
                     switch (soundDef.defName)
                     {
                         case "Pawn_Human_Wounded":
-
                             switch (info.Maker.Cell.GetFirstPawn(info.Maker.Map).gender)
                             {
                                 case Gender.Male:
-                                    soundDef = SoundDef.Named("Pawn_Male1_Wounded");
+                                    switch (LoadedModManager.GetMod<RealisticHumanSounds>().GetSettings<Settings>().selectedMaleSound)
+                                    {
+                                        case Settings.maleSounds.vanilla:
+                                            soundDef = SoundDef.Named("Pawn_Male1_Wounded");
+                                            break;
+                                        case Settings.maleSounds.anime:
+                                            soundDef = SoundDef.Named("Pawn_Male2_Wounded");
+                                            break;
+                                    }
                                     break;
                                 case Gender.Female:
-                                    soundDef = SoundDef.Named("Pawn_Female1_Wounded");
+                                    switch (LoadedModManager.GetMod<RealisticHumanSounds>().GetSettings<Settings>().selectedFemaleSound)
+                                    {
+                                        case Settings.femaleSounds.vanilla:
+                                            soundDef = SoundDef.Named("Pawn_Female1_Wounded");
+                                            break;
+                                        case Settings.femaleSounds.anime:
+                                            soundDef = SoundDef.Named("Pawn_Female2_Wounded");
+                                            break;
+                                    }
                                     break;
                             }
                             return;
@@ -40,10 +55,26 @@ namespace RealisticHumanSoundsPatch
                             switch (info.Maker.Cell.GetFirstPawn(info.Maker.Map).gender)
                             {
                                 case Gender.Male:
-                                    soundDef = SoundDef.Named("Pawn_Male1_Wounded");
+                                    switch (LoadedModManager.GetMod<RealisticHumanSounds>().GetSettings<Settings>().selectedMaleSound)
+                                    {
+                                        case Settings.maleSounds.vanilla:
+                                            soundDef = SoundDef.Named("Pawn_Male1_Death");
+                                            break;
+                                        case Settings.maleSounds.anime:
+                                            soundDef = SoundDef.Named("Pawn_Male2_Death");
+                                            break;
+                                    }
                                     break;
                                 case Gender.Female:
-                                    soundDef = SoundDef.Named("Pawn_Female1_Wounded");
+                                    switch (LoadedModManager.GetMod<RealisticHumanSounds>().GetSettings<Settings>().selectedFemaleSound)
+                                    {
+                                        case Settings.femaleSounds.vanilla:
+                                            soundDef = SoundDef.Named("Pawn_Female1_Death");
+                                            break;
+                                        case Settings.femaleSounds.anime:
+                                            soundDef = SoundDef.Named("Pawn_Female2_Death");
+                                            break;
+                                    }
                                     break;
                             }
                             return;
---
+++ Source/RealisticHumanSounds/Settings.cs
@@ -0,0 +1,78 @@
+﻿using SettingsHelper;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RealisticHumanSounds
+{
+    public class Settings : ModSettings
+    {
+        /// <summary>
+        /// The settings our mod has.
+        /// </summary>
+        public enum maleSounds
+        {
+            vanilla,
+            anime
+        };
+        public enum femaleSounds
+        {
+            vanilla,
+            anime
+        };
+
+        public maleSounds selectedMaleSound = maleSounds.vanilla;
+        public femaleSounds selectedFemaleSound = femaleSounds.vanilla;
+
+        /// <summary>
+        /// The part that writes our settings to file. Note that saving is by ref.
+        /// </summary>
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref selectedMaleSound, "selectedMaleSound");
+            Scribe_Values.Look(ref selectedFemaleSound, "selectedFemaleSound");
+            base.ExposeData();
+        }
+    }
+
+    public class RealisticHumanSounds : Mod
+    {
+        /// <summary>
+        /// A reference to our settings.
+        /// </summary>
+        Settings settings;
+
+        /// <summary>
+        /// A mandatory constructor which resolves the reference to our settings.
+        /// </summary>
+        /// <param name="content"></param>
+        public RealisticHumanSounds(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<Settings>();
+        }
+
+        /// <summary>
+        /// The (optional) GUI part to set your settings.
+        /// </summary>
+        /// <param name="inRect">A Unity Rect with the size of the settings window.</param>
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+            listing.AddLabeledSlider("Male soundpack", ref settings.selectedMaleSound);
+            listing.AddLabeledSlider("Female soundpack", ref settings.selectedFemaleSound);
+            listing.End();
+            settings.Write();
+        }
+
+        /// <summary>
+        /// Override SettingsCategory to show up in the list of settings.
+        /// Using .Translate() is optional, but does allow for localisation.
+        /// </summary>
+        /// <returns>The (translated) mod name.</returns>
+        public override string SettingsCategory()
+        {
+            return "Realistic Human Sounds";
+        }
+    }
+}
